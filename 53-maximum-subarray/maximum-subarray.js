c8e04b727e3f4b56ef1c6b959def290c24028088
imsauravsingh <imsauravsingh@gmail.com>
2026-02-24 15:54:18 +0530

    Time: 2 ms (70.63%) | Memory: 64.1 MB (49.06%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -8,7 +8,7 @@ var maxSubArray = function(nums) {
 
     for(let i=1; i<nums.length; i++){
         currentSum = Math.max(nums[i], currentSum + nums[i]);
-        maxSum = Math.max(currentSum, maxSum);
+        maxSum = Math.max(maxSum, currentSum);
     }
 
     return maxSum;
