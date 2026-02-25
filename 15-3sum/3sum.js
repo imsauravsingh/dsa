e42567663764f2057b5091268523dc6db5fd7043
imsauravsingh <imsauravsingh@gmail.com>
2026-02-25 19:30:31 +0530

    Time: 35 ms (65.88%) | Memory: 71.6 MB (88.12%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {    
+    const result = [];
+    nums.sort((a,b)=>a-b);
+
+    for(let i=0; i<nums.length; i++){
+
+        if(i>0 && nums[i]==nums[i-1]) continue;
+
+        let left = i+1;
+        let right = nums.length - 1;
+
+        while(left<right){
+
+            const val = nums[i] + nums[left] + nums[right];
+
+            if(val===0){
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+
+                while(left<right && nums[left]==nums[left-1]) left++;
+                while(left<right && nums[right]==nums[right+1]) right--;
+            } else if(val>0){
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+    return result;
+};
