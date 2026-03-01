7c14f1770be8c8be4eb2b349aba9b95cdeb94d51
imsauravsingh <imsauravsingh@gmail.com>
2026-03-01 13:44:08 +0530

    Time: 9 ms (97.88%) | Memory: 72.7 MB (89.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -6,12 +6,13 @@ var containsDuplicate = function(nums) {
 
     const obj = new Set();
 
-    for(let i=0; i<nums.length; i++){
-        if(obj.has(nums[i])) return true;
+    for(let i=0; i< nums.length; i++){
+
+        if(obj.has(nums[i])) return true
 
         obj.add(nums[i]);
     }
 
     return false;
-    
+
 };
