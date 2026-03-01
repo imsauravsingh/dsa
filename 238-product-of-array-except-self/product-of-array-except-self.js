37516a3b7df4348ba3883c6306b96f570ffbcd02
imsauravsingh <imsauravsingh@gmail.com>
2026-03-01 13:57:01 +0530

    Time: 7 ms (55.63%) | Memory: 67.8 MB (92.93%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -5,22 +5,19 @@
 var productExceptSelf = function(nums) {
 
     const ans = [1];
+
     for(let i=1; i<nums.length; i++){
         ans[i] = ans[i-1] * nums[i-1];
     }
-
+    
+    
     let suffix = 1;
-    for(let i=nums.length-2; i>=0; i--){
-        suffix = suffix * nums[i+1];
+    for(let i=nums.length-1;  i>=0;  i--){
         ans[i] = ans[i] * suffix;
+        suffix = suffix * nums[i];
     }
 
-    // const arr = [];
-    // for(let i=0; i<nums.length; i++){
-    //     arr[i] = prefix[i] * suffix[i];
-    // }
 
     return ans;
-
     
 };
