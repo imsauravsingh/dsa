90f48b1225bf056e2a451f279bfbee4613994078
imsauravsingh <imsauravsingh@gmail.com>
2026-03-01 13:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 65.5 MB (35.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,21 +3,20 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-
     let buy = 0;
     let sell = 1;
-
     let result = 0;
 
     while(sell<prices.length){
-        if(prices[buy] > prices[sell]){
-            buy=sell;
+        if(prices[buy]>prices[sell]){
+            buy = sell;
         }else{
-            result = Math.max(result, prices[sell] - prices[buy]);
+            result = Math.max(result, prices[sell]-prices[buy]);
         }
         sell++;
     }
 
     return result;
+
     
 };
