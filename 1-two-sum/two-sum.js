0344e24b51c56019884c17057a955f12545fb14b
imsauravsingh <imsauravsingh@gmail.com>
2026-03-05 16:36:03 +0530

    Time: 2 ms (69.19%) | Memory: 56.3 MB (16.57%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,16 +5,17 @@
  */
 var twoSum = function(nums, target) {
 
-    const map = new Map();
-    for(let i=0; i<nums.length; i++){
-        const val = target - nums[i];
+    let map = new Map();
+    for(let i=0; i < nums.length; i++){
+
+        const val = target -  nums[i];
 
         if(!map.has(val)){
-            map.set(nums[i], i);                                                                  
+            map.set(nums[i], i);
         }else{
-            const checkIndex = map.get(val);
-            const index = i;
-            return [checkIndex,index]
+            const index = map.get(val);
+            return [i, index];
         }
     }
+
 };
