4dce1fe8a8c86083f66719501dbfb5cadb3e6795
imsauravsingh <imsauravsingh@gmail.com>
2026-03-05 13:35:00 +0530

    Time: 2 ms (71.02%) | Memory: 58.4 MB (32.26%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+
+    let total = nums.reduce((acc,val)=> acc +=val);
+    let left = 0;
+
+    for(let i=0; i<nums.length; i++){
+        let right = total - left - nums[i];
+        if(right === left) return i;
+
+        left +=nums[i];
+    }
+
+    return -1;
+
+
+    
+};
