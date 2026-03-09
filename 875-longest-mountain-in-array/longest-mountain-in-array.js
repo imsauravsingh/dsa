aa3ffd92f1cf548f01147be5193707dcbee1478e
imsauravsingh <imsauravsingh@gmail.com>
2026-03-09 17:33:43 +0530

    Time: 4 ms (35.00%) | Memory: 55.9 MB (97.50%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var longestMountain = function(arr) {
+
+    let count = 0;
+
+    for(let i=1; i<arr.length-1;){
+
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            let left = i; 
+            let right = i;
+
+            while(left>0 && arr[left]>arr[left-1]) left--;
+            while(right<arr.length-1 && arr[right]>arr[right+1])
+            { 
+                right++;
+                i++;
+            }
+            count = Math.max(count, right-left + 1 );
+        }else{
+            i++;
+        }
+    }
+
+    return count;
+    
+};
