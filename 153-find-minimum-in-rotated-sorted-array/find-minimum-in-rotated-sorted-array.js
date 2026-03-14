adf4c30b1451a439e19d95975ddccd4d67f541cc
imsauravsingh <imsauravsingh@gmail.com>
2026-03-14 18:20:26 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (84.48%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let left = 0; 
-    let right = nums.length - 1;
+    let left=0;
+    let right=nums.length - 1;
+    let ans = Infinity;
 
-    while(left < right){
-        const mid = Math.floor((left + right)/2);
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
 
-        if(nums[mid]>nums[right]){
+        if(nums[left]<=nums[mid]){
+            ans = Math.min(ans, nums[left]);
             left = mid + 1;
         }else{
-            right = mid;
+            ans = Math.min(ans, nums[mid]);
+            right = mid - 1;
         }
     }
-
-    return nums[left];
+    return ans;
 };
