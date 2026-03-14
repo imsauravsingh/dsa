79223b697d7b1abe3a0430316b06acc9721d8797
imsauravsingh <imsauravsingh@gmail.com>
2026-03-14 17:35:59 +0530

    Time: 335 ms (5.37%) | Memory: 55.8 MB (44.79%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.js
+++ 152-maximum-product-subarray/maximum-product-subarray.js
@@ -3,18 +3,19 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let maxProd = nums[0];
-    let minProd = nums[0];
-    let result = nums[0];
 
-    for(let i=1; i<nums.length; i++){
+    let max = -Infinity;
 
-        let tempMax = Math.max(nums[i], nums[i] * maxProd, nums[i] * minProd)
-        minProd = Math.min(nums[i], nums[i] * maxProd, nums[i] * minProd);
-        maxProd = tempMax;
-        result = Math.max(result, maxProd);
+    for(let i=0; i<nums.length; i++){
+        let curr = 1;
+        for(let j=i; j<nums.length; j++){
+            curr = curr * nums[j];
+            max = Math.max(max, curr);
+        }
     }
 
-    return result;
-    
+    return max;
 };
+
+// Time complexity: O(n2)
+// Space Complexity: O(1)
