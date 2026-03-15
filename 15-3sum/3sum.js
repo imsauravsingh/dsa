31bf403b2b72d343b651830af393aaded1f86cba
imsauravsingh <imsauravsingh@gmail.com>
2026-03-15 19:45:15 +0530

    Time: 32 ms (83.10%) | Memory: 70.9 MB (95.57%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {    
+var threeSum = function(nums) {
 
     const result = [];
     nums.sort((a,b)=>a-b);
 
-    for(let i=0; i<nums.length-2; i++){
-        let j = i + 1;
+    for(let i=0; i<nums.length; i++){
+        let j = i+1;
         let k = nums.length - 1;
 
         if(i>0 && nums[i]===nums[i-1]) continue;
 
         while(j<k){
-            const total = nums[i] + nums[j] + nums[k];
+            let total = nums[i] + nums[j] + nums[k];
 
             if(total===0){
                 result.push([nums[i], nums[j], nums[k]]);
@@ -24,13 +24,15 @@ var threeSum = function(nums) {
 
                 j++;
                 k--;
+            }else if(total<0){
+                j++;
+            }else{
+                k--;
             }
-            else if(total<0){ j++; }
-            else if(total>0){ k--; }
         }
+
+
     }
-
     return result;
-
 
 };
