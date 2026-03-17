ba81134e21a4b864057471fc4a3c6c6a68fa2192
imsauravsingh <imsauravsingh@gmail.com>
2026-03-17 18:47:37 +0530

    Time: 25 ms (85.12%) | Memory: 66.8 MB (82.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -6,13 +6,17 @@ var groupAnagrams = function(strs) {
     const map = new Map();
 
     for(let i=0; i<strs.length; i++){
-        const sortedStr = strs[i].split('').sort().join('');
-        if(!map.has(sortedStr)){
-            map.set(sortedStr, []);
+        // const sortedStr = strs[i].split('').sort().join('');
+        const count = new Array(26).fill(0);
+        for(let char of strs[i]){
+            count[char.charCodeAt(0)-97]++;
+        }
+        const key = count.join('#'); 
+        if(!map.has(key)){
+            map.set(key, []);
         }
 
-        map.get(sortedStr).push(strs[i]);
-
+        map.get(key).push(strs[i]);
     }
 
     return Array.from(map.values());
