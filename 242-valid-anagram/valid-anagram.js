f453ef73fce7896e6f0cae546569e613f3c775b4
imsauravsingh <imsauravsingh@gmail.com>
2026-03-17 18:02:29 +0530

    Time: 5 ms (95.72%) | Memory: 56.3 MB (66.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -6,10 +6,15 @@
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
 
-    const sval = s.split('').sort().join('');
-    const tval = t.split('').sort().join('');
+    const count = new Array(26).fill(0)
+    for(let i=0; i<s.length; i++){
+        count[s.charCodeAt(i)-97]++;
+        count[t.charCodeAt(i)-97]--;
+    }
 
-    if(sval!==tval) return false
+    for(let num of count){
+        if(num !==0 ) return false;
+    }
 
-    return true;     
+    return true;
 };
