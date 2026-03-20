3ac0589ea0c347b5007b130cdea20f5d17e255b3
imsauravsingh <imsauravsingh@gmail.com>
2026-03-21 03:22:33 +0530

    Time: 2 ms (71.14%) | Memory: 63.2 MB (92.05%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -5,12 +5,19 @@
 var maxSubArray = function(nums) {
     let curr = 0;
     let max = -Infinity;
-    for(let num of nums){
-        curr += num;
-        max = Math.max(max, curr);
-        if(curr<0){
-            curr = 0;
-        }
+    let prefix = 0;
+    let suffix = 0;
+    for(let i=0; i<nums.length; i++){
+
+        if(prefix<0) prefix=0;
+        if(suffix<0) suffix=0;
+
+        prefix +=nums[i];
+        suffix +=nums[nums.length-1-i];
+
+        max = Math.max(prefix, suffix, max);  
+
+        
     }
 
     return max;
