cbb04c88469f005eab33fa84fd90a3fb5feb6924
imsauravsingh <imsauravsingh@gmail.com>
2026-03-22 01:11:57 +0530

    Time: 8 ms (8.57%) | Memory: 56 MB (55.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -8,20 +8,14 @@ var reverseWords = function(s) {
     let words = '';
     while(left<s.length){
         while(left < s.length && s[left]===" "){ 
-            if(words) arr.unshift(words);
+            if(words) arr.push(words);
             left++;
             words='';
         }
         words +=s[left];
         left++;
-        if(left===s.length && words) arr.unshift(words);
+        if(left===s.length && words) arr.push(words);
     }
 
-    return arr.join(' ');
-
-
+    return arr.reverse().join(' ');
 };
-
-function checkStr(char){
-    return /[a-z0-9]/i.test(char);
-}
