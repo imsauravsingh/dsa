f4fb2bbe6614c23647cc91134cae275aaa0c15a3
imsauravsingh <imsauravsingh@gmail.com>
2026-03-22 17:09:54 +0530

    Time: 23 ms (47.51%) | Memory: 61.4 MB (21.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -3,7 +3,7 @@
  * @return {string}
  */
 
-var checkDrome = (i, j, s) => {
+const checkDrome = (i, j, s) => {
 
     while(i>=0 && j<s.length){
         if(s[i]!==s[j]) break;
@@ -14,19 +14,20 @@ var checkDrome = (i, j, s) => {
     return [i+1, j];
 
 }
+
 var longestPalindrome = function(s) {
+    if(!s) return s;
 
-    if(!s.length) return s;
     let max = [0,1];
-
     for(let i=0; i<s.length; i++){
-        const odd = checkDrome(i-1, i, s);
-        const even = checkDrome(i-1, i+1, s);
+        const even = checkDrome(i-1, i, s);
+        const odd = checkDrome(i-1, i+1, s);
 
-        const temp = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-        max = max[1] - max[0] > temp[1] - temp[0] ? max : temp;
+        const curr = odd[1]-odd[0] >  even[1]-even[0]?odd:even;
+        max = curr[1] - curr[0] > max[1] - max[0] ? curr : max;
     }
 
     return s.slice(max[0], max[1]);
     
+   
 };
